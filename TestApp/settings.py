8f02bdf23047cfd04d3114c57253a313e2e6437e
Shalini Kumari <kshalini1410@gmail.com>
2025-10-31 22:18:41 +0530

    Moved SECRET_KEY to environment variable for secure deployment

--- TestApp/settings.py
+++ TestApp/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&)*y9_@_zbn_3eq82%@!wfa&onq!q@6zwpu_s7piqo@vju#u0g"
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
