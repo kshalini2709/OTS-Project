b532d11936b8be1f763c0806629250201c43c58f
Shalini Kumari <kshalini1410@gmail.com>
2025-10-31 23:27:04 +0530

    Updated ALLOWED_HOSTS for Render deployment

--- TestApp/settings.py
+++ TestApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["ots-project.onrender.com"]
 
 
 # Application definition
